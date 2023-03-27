18b3ee3bbe2ea6b4cf9001090fe0e1cd6ad6e6c0
Diyar Çolak <114312886+diyarcolakk@users.noreply.github.com>
2023-03-28 00:28:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-import axios from "axios";
+import axios from "axios"; //Getting axios package from axios.
 
 async function getData(id) {
+  //Getting first request
   const { data: firstRequest } = await axios(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
+  //Getting second request
   const { data: secondRequest } = await axios(
     `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
 
-  const newObj = { firstRequest, Posts: secondRequest };
+  const newObj = { firstRequest, Posts: secondRequest }; // Two request in a same Object that was required
   console.log(newObj);
 }
 
+// I am going to export here because i need to import in index.js
 export default getData;
